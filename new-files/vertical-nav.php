75542f17f84915d9a20b1deaf6af35401984fc6c
biTTeRG1nGeR <82258454+biTTeRG1nGeR@users.noreply.github.com>
2021-04-24 13:20:39 +0530

    application

---
+++ new-files/vertical-nav.php
@@ -0,0 +1,61 @@
+ <!-- Vertical navbar -->
+ <div class="vertical-nav bg-white" id="sidebar">
+        <div class="py-4 px-3 mb-4 bg-light">
+            <div class="media d-flex align-items-center"><img
+                    src="https://res.cloudinary.com/mhmd/image/upload/v1556074849/avatar-1_tcnd60.png" width="65"
+                    class="mr-3 rounded-circle img-thumbnail shadow-sm">
+                <div class="media-body">
+                    <h4 class="m-2">Jason Doe</h4>
+                    <p class="font-weight-light text-muted my-0 m-2">Professor</p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <ul class="nav flex-column bg-white mb-0">
+            <li class="nav-item">
+                <a href="dashboard.html" class="nav-link text-dark font-italic">
+                    <i class="fa fa-th-large mr-3 text-dark fa-fw"></i>
+                    DASHBOARD
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="profile.html" class="nav-link text-dark font-italic">
+                    <i class="fa fa-user-circle mr-3 text-dark fa-fw"></i>
+                    PROFILE
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark font-italic">
+                    <i class="fa  fa-envelope-open mr-3 text-dark fa-fw"></i>
+                    MESSAGES
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark font-italic">
+                    <i class="fa fa-plug mr-3 text-dark fa-fw"></i>
+                    CONNECTIONS
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="application.html" class="nav-link text-dark font-italic">
+                    <i class="fa fa-paperclip mr-3 text-dark fa-fw"></i>
+                    APPLICATIONS
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark font-italic">
+                    <i class="fa fa-question-circle mr-3 text-dark fa-fw"></i>
+                    FORUMS
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="blog.html" class="nav-link text-dark font-italic bg-light">
+                    <i class="fa fa-rss mr-3 text-dark fa-fw"></i>
+                    BLOG
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- End vertical navbar -->
